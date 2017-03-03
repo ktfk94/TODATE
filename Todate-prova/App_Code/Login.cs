b623f15d412e2a2893ebf7185301971d7c9c658a
Sergisd27 <sergisd@hotmail.com>
2017-03-03 15:14:05 +0100

    FUNCIONA PLOX 332017

---
+++ Todate-prova/App_Code/Login.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Login
+/// </summary>
+public class Login
+{
+        public string mail;
+        public string pw;
+        public Login() { }
+}
