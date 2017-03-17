f2c478c2a7a3aec15de1e1bcb94030cbe802b566
Sergisd27 <sergisd@hotmail.com>
2017-03-17 11:36:11 +0100

    Add files via upload

---
+++ Todate-prova/Todate-prova/App_Code/FMong.cs
@@ -0,0 +1,157 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+using MongoDB.Driver.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+/// <summary>
+/// Summary description for FMong
+/// </summary>
+public class FMong
+{
+
+    protected static IMongoClient _client;
+    protected static IMongoDatabase _database;
+    static Usuari user = new Usuari();
+
+    public FMong()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    //static void Main(string[] args)
+    //{
+    //    CallMain(args).Wait();
+    //    Console.ReadLine();
+    //}
+
+
+    //static async Task CallMain(string[] args)
+    //{
+    //    var conString = "mongodb://localhost:27017";
+    //    var Client = new MongoClient(conString);
+    //    var DB = Client.GetDatabase("test");
+    //    var collection = DB.GetCollection<BsonDocument>("store");
+    //}
+
+    static public void preUpload(Usuari usuarioR, string tipo)
+    {
+        user = usuarioR;
+
+        if (tipo == "Insert")
+        {
+            new Task(Insert).Start();
+        }
+
+        if (tipo == "Select")
+        {
+            //new Task(Select).Start();
+        }
+
+
+    }
+
+    static public bool preUploadSelect(string correu, string pw)
+    {
+        bool comp = false;
+        comp = Select(correu, pw);
+        return comp;
+    }
+
+
+    static protected async void Insert()
+    {
+
+        _client = new MongoClient();
+        _database = _client.GetDatabase("prova");
+        var gustos = new BsonArray();
+        string tast = UtilSignUp.formArray(user.tastes);
+        string iv = UtilSignUp.formArray(user.iv);
+
+        /*gustos.Add(new BsonDocument{  //Codi per a posar un array dintre del document
+            {"tastes", tast}
+        });*/
+        var document = new BsonDocument
+            {
+                { "name",  user.name},
+                { "birthdate", user.birthdate},
+                //{ "img", "Blank"},
+                { "sex", user.sex },
+                { "sexWanted", user.sexWanted },
+                { "mail", user.mail },
+                { "pw", user.pw },
+                { "colour", user.colour },
+                { "typeOfHair", user.typeOfHair },
+                { "shape", user.shape},
+                { "tastes", tast },
+                { "sports", user.sports },
+                //{ "birthplace", user.birthplace },
+                //{ "ubication", "blank" },
+                //{ "religion", user.religion },
+                { "civil status", user.civilstatus },
+                { "children", user.children },
+                { "iv", iv },
+            };
+
+        var collection = _database.GetCollection<BsonDocument>("Usuaris");
+        await collection.InsertOneAsync(document);
+    }
+
+    static protected bool Select(string correu, string pw)
+    {
+
+        //_client = new MongoClient();
+        //database = _client.GetDatabase("prova");
+        var client = new MongoClient("mongodb://localhost");
+        var coll = client.GetServer().GetDatabase("prova").GetCollection("Usuaris");
+
+
+        /*gustos.Add(new BsonDocument{  //Codi per a posar un array dintre del document
+            {"tastes", tast}
+        });*/
+        //var collection = _database.GetCollection<BsonDocument>("Usuaris");
+        //var filter = Builders<BsonDocument>.Filter.Eq("name", user.name);
+        //var count = 0;
+        //var batch = new BsonDocument();
+        //using (var cursor = await collection.FindAsync(filter))
+        //{
+        //    while (await cursor.MoveNextAsync())
+        //    {
+        //         batch= cursor.Current;
+        //        foreach (var document in batch)
+        //        {
+        //            // process document
+        //            count++;
+        //        }
+        //    }
+        //}
+
+        var query = Query.EQ("mail", correu);
+        var doc2 = coll.FindOne(query);
+        MongoDB.Bson.BsonValue valueMail = null;
+        MongoDB.Bson.BsonValue valuePw = null;
+        if (doc2 != null)
+        {
+            valueMail = doc2["mail"];
+            valuePw = doc2["pw"];
+        }
+
+        bool comp = false;
+        if (valueMail != null && valuePw != null)
+        {
+            if (valueMail.AsString == correu && valuePw.AsString == pw)
+            {
+                comp = true;
+            }
+        }
+
+
+        return comp;
+    }
+}
---
+++ Todate-prova/Todate-prova/App_Code/UtilSignUp.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+/// <summary>
+/// Summary description for UtilSignUp
+/// </summary>
+public class UtilSignUp
+{
+    public UtilSignUp()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    static public string[] selectedIndexesOfCheckBoxList(CheckBoxList chkList)
+    {
+        List<string> selectedIndexes = new List<string>();
+
+        foreach (ListItem item in chkList.Items)
+        {
+            if (item.Selected)
+            {
+                selectedIndexes.Add(item.Value);
+            }
+        }
+
+        return selectedIndexes.ToArray();
+    }
+
+
+    static protected void llegirFitxer()
+    {
+        String[] genti = new String[194];
+        ArrayList gentis = new ArrayList();
+        try
+        {   // Open the text file using a stream reader.
+            using (StreamReader sr = new StreamReader("Altres/Documents/gentilicios.txt"))
+            {
+                // Read the stream to a string, and write the string to the console.
+                String line = sr.ReadLine();
+                gentis.Add(line);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("The file could not be read:");
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    static public string formArray(string[] list)
+    {
+        string concat = "";
+        for (int i = 0; i < list.Length; i++)
+        {
+
+            if (!(list[i] == list.Last()))
+            {
+                concat += list[i] + "_";
+            }
+            else
+            {
+                concat += list[i];
+            }
+        }
+        return concat;
+    }
+}
